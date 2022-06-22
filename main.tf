78c159031558e62a0d87eb33fb9bd1c7f13a14fa
SuhelMalik <shanu.ironsmith@gmail.com>
2022-06-22 03:12:44 -0400

    should use the subnet specified from the vpc

--- main.tf
+++ main.tf
@@ -68,9 +68,13 @@ data "aws_ami" "amazon-linux-2" {
  }
 }
 
+data "aws_subnet_ids" "subnet_ids" {
+  vpc_id = data.aws_vpc.main.id
+}
 
 resource "aws_instance" "my_server" {
   ami           = "${data.aws_ami.amazon-linux-2.id}"
+  subnet_id = tolist(data.aws_subnet_ids.subnet_ids.ids)[0]
   instance_type = var.instance_type
   key_name = "${aws_key_pair.deployer.key_name}"
   vpc_security_group_ids = [aws_security_group.sg_my_server.id]
